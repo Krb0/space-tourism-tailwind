2fb11d2f96d561867bb71952c55fb2274dcc085a
Krb0 <maurodavidcaceres@gmail.com>
2022-04-20 15:36:51 -0300

    overlay added

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,12 +1,30 @@
+import { useLocation } from "react-router-dom";
 import NavBar from "./NavBar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { pathname } = useLocation();
   return (
-    <div className="bg-cover min-w-screen min-h-screen bg-right bg-home">
-      <NavBar />
-      {children}
+    <div
+      className={`bg-cover min-w-screen min-h-screen bg-right bg-blur bg-${normalizePath(
+        pathname
+      )} pb-12 `}
+    >
+      <div className="fixed w-screen h-screen  bg-[rgba(0,0,0,0.2)] z-[0] "></div>
+      <div className="relative z-[4] ">
+        <NavBar />
+        <main>{children}</main>
+      </div>
     </div>
   );
 };
 
 export default Layout;
+
+const normalizePath = (pathname: any) => {
+  const path = pathname.split("/").find((p: any) => p !== "");
+  if (path) {
+    return path;
+  } else {
+    return "home";
+  }
+};
