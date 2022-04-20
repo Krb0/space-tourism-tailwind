7c7501af5f43a51d1ee0063060bf69f093bdf68d
Krb0 <maurodavidcaceres@gmail.com>
2022-04-20 15:36:34 -0300

    home fixes

--- src/components/Home/RightPanel/index.tsx
+++ src/components/Home/RightPanel/index.tsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 const RightPanel = () => {
   return (
-    <button className="rounded-full font-heading w-[274px] h-[274px] bg-white flex items-center justify-center cursor-pointer select-none text-[25px] mt-28  relative group">
-      <div className="absolute h-[274px] w-[274px] rounded-full bg-[rgba(255,255,255,0.1)] group-hover:h-[374px] group-hover:w-[374px] transition-all "></div>
-      <span className="flex items-center justify-center w-full h-full bg-white rounded-full z-[1] ">
-        EXPLORE
-      </span>
-    </button>
+    <Link to="destination">
+      <button className="rounded-full font-heading w-[200px] xl:w-[274px] h-[200px] xl:h-[274px] bg-white flex items-center justify-center cursor-pointer select-none text-[25px] mt-28  relative group">
+        <div className="absolute xl:h-[274px] xl:w-[274px] h-[200px] w-[200px] rounded-full bg-[rgba(255,255,255,0.1)] xl:group-hover:h-[374px] xl:group-hover:w-[374px] group-hover:h-[260px] group-hover:w-[260px] transition-all " />
+        <span className="flex items-center justify-center w-full h-full bg-white rounded-full z-[1] ">
+          EXPLORE
+        </span>
+      </button>
+    </Link>
   );
 };
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,7 @@ import RightPanel from "../components/Home/RightPanel";
 
 const Home = () => {
   return (
-    <div className="flex items-center justify-between px-20 xl:px-44 pt-44">
+    <div className="flex items-center justify-between px-20 xl:px-44 pt-44 flex-col xl:flex-row text-center xl:text-left">
       <LeftPanel />
       <RightPanel />
     </div>
