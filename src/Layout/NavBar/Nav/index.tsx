112d1039488d5e9b7f9a490615871bd9a638dc6b
Krb0 <maurodavidcaceres@gmail.com>
2022-04-22 22:32:42 -0300

    nav button added

--- src/Layout/NavBar/Nav/index.tsx
+++ src/Layout/NavBar/Nav/index.tsx
@@ -1,26 +1,70 @@
 import NavLink from "./NavLink";
-
-const Nav = () => {
+import HamburgerIcon from "../../../assets/shared/icon-hamburger.svg";
+import OpenedIcon from "../../../assets/shared/icon-close.svg";
+import { useState } from "react";
+const Nav = ({ menu }: { menu: boolean }) => {
   return (
-    <nav className="font-condensed text-white flex gap-14 backdrop-blur-[82px] h-[90px] items-center pr-10 xl:pr-60 pl-12 xl:pl-24 bg-[#ffffff02]">
-      <NavLink path="/">
-        <span className="hidden xl:block">00</span>
-        HOME
-      </NavLink>
-      <NavLink path="/destination">
-        <span className="hidden xl:block">01</span>
-        DESTINATION
-      </NavLink>
-      <NavLink path="/crew">
-        <span className="hidden xl:block">02</span>
-        CREW
-      </NavLink>
-      <NavLink path="/technology">
-        <span className="hidden xl:block">03</span>
-        TECHNOLOGY
-      </NavLink>
-    </nav>
+    <>
+      <nav className="hidden lg:flex font-condensed text-white gap-14 backdrop-blur-[82px] h-[90px] items-center pr-10 xl:pr-60 pl-12 xl:pl-24 bg-[#ffffff02]">
+        <NavLink path="/">
+          <span className="hidden xl:block">00</span>
+          HOME
+        </NavLink>
+        <NavLink path="/destination">
+          <span className="hidden xl:block">01</span>
+          DESTINATION
+        </NavLink>
+        <NavLink path="/crew">
+          <span className="hidden xl:block">02</span>
+          CREW
+        </NavLink>
+        <NavLink path="/technology">
+          <span className="hidden xl:block">03</span>
+          TECHNOLOGY
+        </NavLink>
+      </nav>
+      <nav className="flex lg:hidden flex-col fixed right-0 top-0 backdrop-blur-[82px] h-screen w-[200px] ">
+        {menu && (
+          <ul className="text-white">
+            <NavLink path="/">
+              <span className="hidden xl:block">00</span>
+              HOME
+            </NavLink>
+            <NavLink path="/destination">
+              <span className="hidden xl:block">01</span>
+              DESTINATION
+            </NavLink>
+            <NavLink path="/crew">
+              <span className="hidden xl:block">02</span>
+              CREW
+            </NavLink>
+            <NavLink path="/technology">
+              <span className="hidden xl:block">03</span>
+              TECHNOLOGY
+            </NavLink>
+          </ul>
+        )}
+      </nav>
+    </>
   );
 };
 
 export default Nav;
+
+export const HamburgerButton = ({
+  menu,
+  setMenu,
+}: {
+  menu: boolean;
+  setMenu: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  return (
+    <button className="w-8 h-8 z-20" onClick={() => setMenu(!menu)}>
+      <img
+        src={menu ? OpenedIcon : HamburgerIcon}
+        className="w-8 h-8"
+        alt="menu button"
+      />
+    </button>
+  );
+};
--- src/Layout/NavBar/index.tsx
+++ src/Layout/NavBar/index.tsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/shared/logo.svg";
-import Nav from "./Nav";
+import Nav, { HamburgerButton } from "./Nav";
 
 const NavBar = () => {
+  const [menu, setMenu] = useState<boolean>(false);
   return (
-    <header className="pt-[2rem] flex items-center justify-between pl-20 flex-wrap">
+    <header className="pt-[2rem] flex items-center justify-between pl-20 flex-wrap pr-10 lg:pr-0">
       <Link to="/">
         <img src={Logo} alt="logo" className="h-[48px] w-[48px] " />
       </Link>
       <hr className="line xl:block" />
-      <Nav />
+      <HamburgerButton menu={menu} setMenu={setMenu} />
+      <Nav menu={menu} />
     </header>
   );
 };
